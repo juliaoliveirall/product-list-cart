4b9e9eea2fc2fe19c64a5cb2e59ee09633a70fba
Júlia Oliveira <limajulia2912@gmail.com>
2024-10-09 21:44:19 -0300

    quarto commit

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,11 @@ import { Product } from "./entities/product";
 import data from "../data.json";
 import { Cart } from "./entities/cart";
 
-const product1 = new Product("Banana", "Fruta", 10, "http://example.com");
-const product2 = new Product("Mamão", "Fruta", 5, "http://example.com");
-
-const cart = new Cart();
-
-cart.addToCart(product1);
-cart.addToCart(product2);
-console.log(cart);
-console.log(cart.products.length);
-console.log(cart.total);
+for (const product of data) {
+  new Product(
+    product.name,
+    product.category,
+    product.price,
+    product.image.desktop
+  ).toHTML();
+}
--- src/entities/cart.ts
+++ src/entities/cart.ts
@@ -1,24 +1,81 @@
 import { Product } from "./product";
 
 export class Cart {
-  private _products: Product[] = [];
-  private _total: number = 0;
+  private static _products: Product[] = [];
+  private static _orderTotal: number = 0;
+  private static _totalQuantity: number = 0;
 
-  addToCart(product: Product) {
+  static calculateTotal() {
+    this._orderTotal = 0;
+    this._totalQuantity = 0;
+
+    for (const product of this.products) {
+      this._orderTotal += product.total;
+      this._totalQuantity += product.quantity;
+    }
+  }
+
+  static removeProduct(product: Product) {
+    // Remove um produto do carrinho, filtrando pelo produto em questão
+    this._products = this._products.filter((item) => item.id !== product.id);
+    this.calculateTotal();
+  }
+
+  static addToCart(product: Product) {
     const productInCart = this._products.includes(product);
 
     if (!productInCart) {
       this._products.push(product);
     }
 
-    this._total += product.price;
+    this.calculateTotal();
+    this.toHTML();
   }
 
-  get products() {
+  static toHTML() {
+    const cartContainerHTML = document.getElementById("cart-container");
+
+    if (!cartContainerHTML) return;
+
+    const totalQuantityHTML = cartContainerHTML.querySelector(
+      "#total-quantity-text"
+    );
+
+    if (!totalQuantityHTML) return;
+    totalQuantityHTML.textContent = this._totalQuantity.toString();
+
+    let ulProductsHTML = cartContainerHTML.querySelector("ul");
+
+    if (ulProductsHTML) {
+      ulProductsHTML.innerHTML = "";
+    } else {
+      ulProductsHTML = document.createElement("ul");
+    }
+
+    for (const product of this._products) {
+      const liProductHTML = document.createElement("li");
+
+      const productHTML = `
+        <span>${product.name}</span>
+        <div>
+          <span>${product.quantity}x</span>
+          <span>@$${product.price}</span>
+          <span>$${product.total}</span>
+        </div>
+      `;
+
+      liProductHTML.innerHTML = productHTML;
+      ulProductsHTML.appendChild(liProductHTML);
+    }
+
+    cartContainerHTML.appendChild(ulProductsHTML);
+  }
+
+  static get products() {
     return this._products;
   }
 
-  get total() {
-    return this._total;
+  static get total() {
+    return this._orderTotal;
   }
 }
